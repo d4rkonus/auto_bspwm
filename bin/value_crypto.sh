d49733faea7f6fc648a49e0b77e5975e23c86198
d4rkonus <d4rkonus@gmail.com>
2025-11-27 00:20:03 +0000

    Update value_crypto.sh

--- bin/value_crypto.sh
+++ bin/value_crypto.sh
@@ -5,5 +5,4 @@ API="https://api.kraken.com/0/public/Ticker"
 quote=$(curl -sf $API?pair=BTCEUR | jq -r ".result.XXBTZEUR.c[0]")
 quote=$(LANG=C printf "%.2f" "$quote")
 
-formatted=$(echo "$quote" | awk '{printf "%'\''0.2f", $1}' | sed 's/\\./,/;:a;s/\\B[0-9]\\{3\\}\\>/.&/;ta')
-echo -e " $formatted€"
+echo "#1 $quote €" | awk '{printf " %'\''0.2f€", $2}' | sed 's/\,/./;:a;s/\B[0-9]\{3\}\>/.&/;ta'
